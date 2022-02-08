4488b0205b1094008b563346e8b8d4645b46be10
Denilson Lemus <dlemus@harding.edu>
2022-02-08 13:34:49 -0600

    Change format to 1 decimal place
    Google's temperature converter uses one decimal place only

--- App/app/src/main/java/com/devnilson/temperatureconverter/TemperatureConverter.kt
+++ App/app/src/main/java/com/devnilson/temperatureconverter/TemperatureConverter.kt
@@ -4,9 +4,9 @@ class TemperatureConverter(var degreesToConvert: Int, var toFahrenheit: Boolean)
     val result: String
         get() {
             if (toFahrenheit) {
-                return "%.2f".format((degreesToConvert * 1.8) + 32)
+                return "%.1f".format((degreesToConvert * 1.8) + 32)
             }
 
-            return "%.2f".format((degreesToConvert - 32) * .5556)
+            return "%.1f".format((degreesToConvert - 32) * .5556)
         }
 }
