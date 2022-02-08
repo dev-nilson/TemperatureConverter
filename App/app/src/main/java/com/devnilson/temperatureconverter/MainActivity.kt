c0baabe8d72e8ccac38b8a35959ea06085b058e8
Denilson Lemus <dlemus@harding.edu>
2022-02-07 20:17:56 -0600

    Add conversion/calculation function

--- App/app/src/main/java/com/devnilson/temperatureconverter/MainActivity.kt
+++ App/app/src/main/java/com/devnilson/temperatureconverter/MainActivity.kt
@@ -1,21 +1,40 @@
 package com.devnilson.temperatureconverter
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var degreesToConvert: EditText
-    private lateinit var result: TextView
+    private lateinit var degreesToConvertEditText: EditText
+    private lateinit var conversionUnitRadioGroup: RadioGroup
+    private lateinit var resultTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        degreesToConvert = findViewById(R.id.degrees_edit_text)
-        result = findViewById(R.id.result_text_view)
+        degreesToConvertEditText = findViewById(R.id.degrees_edit_text)
+        resultTextView = findViewById(R.id.result_text_view)
+        conversionUnitRadioGroup = findViewById(R.id.temperature_unit_radio_group)
     }
 
-    
+    fun calculateClick(view: View) {
+        val degreesToConvert = degreesToConvertEditText.text.toString()
+        val degrees = degreesToConvert.toIntOrNull() ?: 0
+        val toFahrenheit = when (conversionUnitRadioGroup.checkedRadioButtonId) {
+            R.id.celcius_radio_button -> true
+            else -> false
+        }
+
+        val conversion = TemperatureConverter(degrees, toFahrenheit)
+        val result = conversion.result
+
+        val resultText = getString(R.string.result, result)
+        resultTextView.setText(resultText)
+    }
 }
--- App/app/src/main/java/com/devnilson/temperatureconverter/TemperatureConverter.kt
+++ App/app/src/main/java/com/devnilson/temperatureconverter/TemperatureConverter.kt
@@ -1,12 +1,12 @@
 package com.devnilson.temperatureconverter
 
-class TemperatureConverter(var degreesToConvert: Float, var toFahrenheit: Boolean) {
-    val result: Double
+class TemperatureConverter(var degreesToConvert: Int, var toFahrenheit: Boolean) {
+    val result: String
         get() {
             if (toFahrenheit) {
-                return (degreesToConvert * 1.8) + 32;
+                return "%.2f".format((degreesToConvert * 1.8) + 32)
             }
 
-            return (degreesToConvert - 32) * .5556
+            return "%.2f".format((degreesToConvert - 32) * .5556)
         }
 }
